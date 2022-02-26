56fbcb066701b30369ec91fc6e3de5b315b86fef
Tanakorn Paorat <tanakorn.paorat@gmail.com>
2022-02-26 21:33:51 +0700

    #4 feat(product-detail): Add mock product data

--- e-commerce-system/src/main/java/io/github/korntkp/ecommercesystem/ECommerceSystemApplication.java
+++ e-commerce-system/src/main/java/io/github/korntkp/ecommercesystem/ECommerceSystemApplication.java
@@ -1,13 +1,28 @@
 package io.github.korntkp.ecommercesystem;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.PostConstruct;
+
 @SpringBootApplication
 public class ECommerceSystemApplication {
+    @Autowired
+    private ProductRepository productRepository;
 
-	public static void main(String[] args) {
-		SpringApplication.run(ECommerceSystemApplication.class, args);
-	}
+    @PostConstruct
+    public void xyz() {
+        Product product1 = new Product(1, "Samsung S9", "Samsung Galaxy S9", "This is Android mobile phone.");
+        Product product2 = new Product(2, "Apple Pencil", "Apple Pencil", "This is Apple Pencil.");
+        Product product3 = new Product(3, "Samsung Note 10", "Samsung Galaxy Note10", "This is Android mobile phone with pencil.");
+        productRepository.save(product1);
+        productRepository.save(product2);
+        productRepository.save(product3);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(ECommerceSystemApplication.class, args);
+    }
 
 }
