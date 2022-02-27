466a7eb4a478b43b874aacf03203f45cd8e5a58b
Tanakorn Paorat <tanakorn.paorat@gmail.com>
2022-02-28 00:57:16 +0700

    #14 feat(customer): Add fields in customer entity

--- e-commerce-system/src/main/java/io/github/korntkp/ecommercesystem/customer/Customer.java
+++ e-commerce-system/src/main/java/io/github/korntkp/ecommercesystem/customer/Customer.java
@@ -1,4 +1,45 @@
 package io.github.korntkp.ecommercesystem.customer;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
 public class Customer {
+
+    @Id
+    private String id;
+    private String firstName;
+    private String lastName;
+
+    public Customer() {
+    }
+
+    public Customer(String id, String firstName) {
+        this.id = id;
+        this.firstName = firstName;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
 }
